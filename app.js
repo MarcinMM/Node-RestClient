263e7d2fdd07755d20782a3e440937808e0b6de6
Marcin Manek <marcin@shopigniter.com>
2012-03-28 17:20:01 -0700

    Get and post work. Got JSON to display nicely. Got status code. Got simple auth to work.

--- app.js
+++ app.js
@@ -3,33 +3,102 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+restclient = {
 
-var app = module.exports = express.createServer();
+	init: function() {
+		rc = restclient;
+		rc.express = require('express');
+		rc.restler = require('restler');
+		rc.app = rc.express.createServer();
 
-// Configuration
+		rc.app.configure(function(){
+			rc.app.set('views', __dirname + '/views');
+			rc.app.set('view engine', 'jade');
+			rc.app.use(rc.express.bodyParser());
+			rc.app.use(rc.express.methodOverride());
+			rc.app.use(rc.express.static(__dirname + '/public'));
+		});
+	    rc.app.configure('development', function(){
+	      rc.app.use(rc.express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	    });
+	    
+	    rc.app.configure('production', function(){
+	      rc.app.use(rc.express.errorHandler()); 
+	    });
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+		rc.app.listen(3000); 
+		console.log("Express server listening on port %d in %s mode", rc.app.address().port, rc.app.settings.env);
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+	    rc.app.post('/', function(req, res) {
+	    	var options = {
+	    		parser: rc.restler.parsers.json,
+	    		username: req.body.username, 
+	    		password: req.body.password
+			};
+			
+			if (req.body.uri_method == 'get') {
+		    	rc.restler.json(req.body.uri, {}, options).
+		    		on('complete', function(data, response) {
+				        res.render('index', {
+				          	title: 'Rest Client Post',
+				          	content: 'Rest Client Post',
+				          	uri: req.body.uri,
+				          	content: req.body.content,
+				          	header: req.body.header,
+				          	response: JSON.stringify(JSON.parse(response.rawEncoded), undefined, 2),
+				          	statusCode: response.statusCode,
+				          	username: req.body.username,
+				          	password: req.body.password,
+				          	uri_method: req.body.uri_method
+				        });
+					});
+	    	} else if (req.body.uri_method == 'post' && (req.body.content.length > 0)) {
+	    		var parsedInput;
+	    		try {
+	    			parsedInput = JSON.parse(req.body.content);
+			    	rc.restler.postJson(req.body.uri, parsedInput, options).
+		    		on('complete', function(data, response) {
+				        res.render('index', {
+				          	title: 'Rest Client Post',
+				          	content: 'Rest Client Post',
+				          	uri: req.body.uri,
+				          	content: req.body.content,
+				          	header: req.body.header,
+				          	response: JSON.stringify(JSON.parse(response.rawEncoded), undefined, 2), 
+				          	statusCode: response.statusCode,
+				          	username: req.body.username,
+				          	password: req.body.password,
+				          	uri_method: req.body.uri_method
+				        });
+					});
+		    	}
+		    	catch (e)
+		    	{
+		    		console.log('not json');
+		    	}
+			}
+    	});
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+	    rc.app.get('/', function(req, res) {
+	        res.render('index', {
+	          	title: 'Rest Client',
+	          	content: 'Rest Client',
+	          	uri: '',
+	          	content: '',
+	          	header: {},
+	          	response: '',
+	          	statusCode: '',
+	          	username: '',
+	          	password: '',
+	          	uri_method: 'get'
+	        });
+	    });
+	},
 
-// Routes
+	render: function(content, response) {
 
-app.get('/', routes.index);
+	}
+}
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var rc = restclient;
+rc.init();
